964341633a223948bb2e43cadc5dbea74255b8ae
sohannaik5648987 <53473526+sohannaik5648987@users.noreply.github.com>
2020-07-01 16:01:33 +0530

    Create SUMOFTWONUMBERS.cpp

---
+++ SUMOFTWONUMBERS.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+	int a,b,sum=0,*m,*n,p;
+	cout<<"Enter The First Number: ";
+	cin>>a;
+	m=&a;
+	cout<<"Enter The Second Number: ";
+	cin>>b;
+	n=&b;
+	sum=*m+*n;
+	
+	cout<<"Sum Of Two Numbers Is: "<<sum;
+}
